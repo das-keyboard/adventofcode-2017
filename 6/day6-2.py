cc8af17ada77eb3ac76bad4de792f2c85f24560e
das_keyboard <daskeyboard@protonmail.com>
2017-12-06 09:50:40 +0100

    Completed Day 6

---
+++ 6/day6-2.py
@@ -0,0 +1,21 @@
+def process(uri: str):
+    f = open(uri, 'r')
+    stack = [int(x) for x in f.readline().replace('\n', '').strip().split('\t')]
+    prev_sets = []
+    steps = 0
+    while str(stack) not in prev_sets:
+        prev_sets.append(str(stack))
+        maxb = max(stack)
+        index = stack.index(maxb)
+        stack[index] = 0
+        for i in range(maxb):
+            index = (index + 1) if index != len(stack) - 1 else 0
+            stack[index] += 1
+        steps += 1
+    return steps - prev_sets.index(str(stack))
+
+
+print(process('input_t'))
+print(process('input'))
+
+
